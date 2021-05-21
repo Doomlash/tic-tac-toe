7db3da1cc7297a50061ea2927ee6ba1b386f953b
Emiliano Vilche <31260215+Doomlash@users.noreply.github.com>
2021-05-20 23:48:52 -0300

    cambiado el archivo de prolog

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -1,5 +1,5 @@
 :- module(proylcc,
-	[  
+	[
 		put/8
 	]).
 
@@ -23,19 +23,100 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
 %
 % put(+Contenido, +Pos, +PistasFilas, +PistasColumnas, +Grilla, -GrillaRes, -FilaSat, -ColSat).
 %
+put(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla, 1, 1):-
+    putAux(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla),
+    isRowCorrect(RowN,PistasFilas,NewGrilla),
+    isColumnCorrect(ColN,PistasColumnas,NewGrilla).
 
-put(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla, 0, 0):-
+put(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla, 1, 0):-
+    putAux(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla),
+    isRowCorrect(RowN,PistasFilas,NewGrilla).
+
+put(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla, 0, 1):-
+    putAux(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla),
+    isColumnCorrect(ColN,PistasColumnas,NewGrilla).
+
+put(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla, 0, 0):-
+    putAux(Contenido, [RowN, ColN], PistasFilas, PistasColumnas, Grilla, NewGrilla).
+
+putAux(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla):-
 	% NewGrilla es el resultado de reemplazar la fila Row en la posición RowN de Grilla
 	% (RowN-ésima fila de Grilla), por una fila nueva NewRow.
-	
+
 	replace(Row, RowN, NewRow, Grilla, NewGrilla),
 
 	% NewRow es el resultado de reemplazar la celda Cell en la posición ColN de Row por _,
 	% siempre y cuando Cell coincida con Contenido (Cell se instancia en la llamada al replace/5).
 	% En caso contrario (;)
-	% NewRow es el resultado de reemplazar lo que se que haya (_Cell) en la posición ColN de Row por Conenido.	 
-	
+	% NewRow es el resultado de reemplazar lo que se que haya (_Cell) en la posición ColN de Row por Conenido.
+
 	(replace(Cell, ColN, _, Row, NewRow),
-	Cell == Contenido 
+	Cell == Contenido
 		;
 	replace(_Cell, ColN, Contenido, Row, NewRow)).
+
+
+getPos(0,[X|_],X).
+getPos(N,[_|XL],X):-
+    N > 0,
+    NS is N - 1,
+    getPos(NS,XL,X).
+
+
+invertirAux([],L,L).
+invertirAux([X|RL],LAux,LI):-
+    invertirAux(RL,[X|LAux],LI).
+
+invertirLista(Lista,ListaInvertida):-
+    invertirAux(Lista,[],ListaInvertida).
+
+countSequence(0,NewFila,NewFila).
+countSequence(Pista,["#"|RFila],NewFila):-
+    Pista > 0,
+    PistaS is Pista - 1,
+    countSequence(PistaS,RFila,NewFila).
+
+countSquares([],[]).
+countSquares([],["X",RCeldas]):-
+    countSquares([],RCeldas).
+countSquares([],["_",RCeldas]):-
+    countSquares([],RCeldas).
+countSquares(Pistas,["X"|RCeldas]):-
+    countSquares(Pistas,RCeldas).
+countSquares(Pistas,["_"|RCeldas]):-
+    countSquares(Pistas,RCeldas).
+countSquares([Pista|RPistas],["#"|RCeldas]):-
+    countSequence(Pista,["#"|RCeldas],NewFila),
+    countSquares(RPistas,NewFila).
+
+isRowCorrect(RowN,PistasFilas,Grilla):-
+    %obtengo la fila de la grilla y las pistas para esa fila
+    getPos(RowN,PistasFilas,PistasN),
+    getPos(RowN,Grilla,Fila),
+    countSquares(PistasN,Fila).
+
+getColAux(_,[],Col,Col).
+getColAux(ColN,[Fila|RFilas],ColAux,Columna):-
+    getPos(ColN,Fila,Celda),
+    getColAux(ColN,RFilas,[Celda|ColAux],Columna).
+
+getCol(ColN,Grilla,Columna):-
+    getColAux(ColN,Grilla,[],ColInvertida),
+    invertirLista(ColInvertida,Columna).
+
+isColumnCorrect(ColN,PistasCol,Grilla):-
+    getPos(ColN,PistasCol,PistasN),
+    getCol(ColN,Grilla,Columna),
+    countSquares(PistasN,Columna).
+
+
+
+
+
+
+
+
+
+
+
+
